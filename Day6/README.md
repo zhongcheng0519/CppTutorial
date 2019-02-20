c622944a6751ecb14e326eddec75cea127f35b7d
zhongc-office-u18 <zhongcheng0519@163.com>
2019-02-20 08:36:14 +0800

    add Day6

---
+++ Day6/README.md
@@ -0,0 +1,23 @@
+# Day 6
+
+Functions
+
+**Goal**:
+
+1. Learn what is a function signature.
+2. Learn the difference between function declaration and function define.
+2. Learn what is a default parameter (by self).
+2. Learn what is a function pointer.
+
+
+**Keypoints**:
+
+1. Signature: `int foo(int x, int y);`.
+2. Declaration can come up any times, however function define can only show up once.
+3. Function pointer is import in callback implementation.
+
+
+**Assignments**:
+
+Implement a simple calculator. Make the `main.cpp` available.
+
---
+++ Day6/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    double first, second;
+    char op;
+    cout << "Please input first operand: ";
+    cin >> first;
+    cout << "Please input operator: (+/-/*//)";
+    cin >> op;
+    cout << "Please input second operand: ";
+    cin >> second;
+    
+    cout << "What you will calculate is: " << 
+        first << " " << op << " " << second << endl;
+
+    double result;
+
+    OpFunc func;
+    switch(op)
+    {
+    case '+':
+        func = add;
+        break;
+    case '-':
+        func = sub;
+        break;
+    case '*':
+        func = mul;
+        break;
+    case '/':
+        func = div;
+        break;
+    default:
+        cerr << "No such operator" << endl;
+        return -1;
+    }
+
+    cout << "Result = " << func(first, second) << endl;
+
+    return 0;
+}
+
