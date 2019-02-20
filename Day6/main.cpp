8f90f88933d2f2608165c7170824b1b98078783a
wangxw <18744014509@163.com>
2019-02-20 21:12:42 +0800

    calculator done

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -1,8 +1,21 @@
-/////
+
 #include <iostream>
 
 using namespace std;
 
+typedef double (*OpFunc)(double,double);
+double add(double x,double y){
+	return x+y;
+}
+double sub(double x,double y){
+	return x-y;
+}
+double mul(double x,double y){
+	return x*y; 
+}
+double div(double x,double y){
+	return x/y;
+}
 
 int main()
 {
